b1fbf7fb856e8c6397c20a47d60e3bd6275f5caa
Andrey Kosinskiy <kosinskiyandrey@ukr.net>
2020-03-05 15:40:40 +0200

    with erorr in added content

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -9,6 +9,7 @@ from .forms import ModuleFormSet
 from django.forms.models import modelform_factory
 from django.apps import apps
 from .models import Module, Content
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 # Create your views here.
 class OwnerMixin(object):
     def get_queryset(self):
@@ -82,7 +83,7 @@ class ContentCreateUpdateView(TemplateResponseMixin, View):
         return Form(*args,**kwargs)
 
     def dispatch(self, request,module_id, model_name, id = None):
-        self.module = get_object_or_404(Module, id = module.id, course__owner = request.user)
+        self.module = get_object_or_404(Module, id = module_id, course__owner = request.user)
         self.model = self.get_model(model_name)
         if id:
             self.obj = get_object_or_404(self.model, id = id , owner = request.user)
@@ -102,3 +103,31 @@ class ContentCreateUpdateView(TemplateResponseMixin, View):
                 Content.objects.create(module = self.module, item = obj)
             return redirect('module_content_list', self.module.id)
         return self.render_to_response({'form':form,'object':self.obj})
+
+
+class ContentDeleteView(View):
+    def post(self, request, id):
+        content = get_object_or_404(Content, id = id, module__course__owner = request.user)
+        module = content.module
+        content.item.delete()
+        content.delete()
+        return redirect('module_content_list', module.id)
+
+class ModuleContentListView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/content_list.html'
+
+    def get(self, request, module_id):
+        module = get_object_or_404(Module, id = module_id, course__owner = request.user)
+        return self.render_to_response({'module':module}) 
+
+class ModuleOrderView(CsrfExemptMixin,JsonRequestResponseMixin,View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(id = id, course__owner = request.user).update(order=order)
+        return self.render_json_response({'saved':"OK"})
+
+class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id,module__course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
